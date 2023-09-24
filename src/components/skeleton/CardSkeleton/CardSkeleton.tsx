91e70a447680c6fa62014007d288fef304714e0d
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-09-24 14:20:49 +0300

    Поправил скелетон карточки

--- src/components/skeleton/CardSkeleton/CardSkeleton.tsx
+++ src/components/skeleton/CardSkeleton/CardSkeleton.tsx
@@ -6,16 +6,19 @@ import { Counter } from '@components/ui/Counter/Counter';
 import { Icons } from '@components/ui/Icon/Icon';
 
 import classes from './CardSkeleton.module.scss';
+import cardClasses from '@components/ui/Card/Card.module.scss';
 
 interface ICardSkeleton {}
 
 export const CardSkeleton: FC<ICardSkeleton> = (): JSX.Element => (
-    <div className={classes.wrapper}>
-        <TextSkeleton className={classes.heading} rowsCount={1} />
-        <TextSkeleton className={classes.description} rowsCount={3} />
-        <div className={classes.footer}>
-            <Counter icon={Icons.STAR} />
-            <Counter icon={Icons.FORK} />
+    <article className={cardClasses.wrapper}>
+        <div className={cardClasses.content}>
+            <TextSkeleton className={classes.heading} rowsCount={1} />
+            <TextSkeleton className={classes.description} rowsCount={3} />
+            <div className={classes.footer}>
+                <Counter icon={Icons.STAR} />
+                <Counter icon={Icons.FORK} />
+            </div>
         </div>
-    </div>
+    </article>
 );
